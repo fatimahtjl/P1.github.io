1e38e4e2f7f6767f1a69dcf9f14a907ab283b50f
gim <ugisugiman6@gmail.com>
2025-01-24 14:11:00 +0700

    title

--- UIpiece/app/Views/Halaman/dashboard.php
+++ UIpiece/app/Views/Halaman/dashboard.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.tailwindcss.com">
     </script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>Usporty</title>
+    <title>UIpiece</title>
 
 <body>
     <!-- Navbar -->
